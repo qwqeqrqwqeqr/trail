450255b2f7a173fe9a1a0c68c236b357cdcdf359
qwqeqrqwqeqr <rhdtkdxor123@kyonggi.ac.kr>
2022-05-29 13:52:48 +0900

    location drawble 추가 kakao maps 연동 작업

--- app/src/main/java/kr/ac/kgu/app/trail/ui/race/RaceMapFragment.kt
+++ app/src/main/java/kr/ac/kgu/app/trail/ui/race/RaceMapFragment.kt
@@ -1,15 +1,26 @@
 package kr.ac.kgu.app.trail.ui.race
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.media.audiofx.BassBoost
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import dagger.hilt.android.AndroidEntryPoint
 import kr.ac.kgu.app.trail.R
@@ -17,8 +28,8 @@ import kr.ac.kgu.app.trail.data.model.CourseEntry
 import kr.ac.kgu.app.trail.databinding.FragmentRaceMapBinding
 import kr.ac.kgu.app.trail.ui.base.BaseFragment
 import kr.ac.kgu.app.trail.ui.base.viewBinding
-import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapView
+
 
 
 @AndroidEntryPoint
@@ -27,7 +38,7 @@ class RaceMapFragment : BaseFragment<RaceMapViewModel, CourseEntry>(
     RaceMapViewModel::class.java
 ) {
 
-
+    private lateinit var mapView : MapView
     private val binding by viewBinding(FragmentRaceMapBinding::bind)
 
 
@@ -35,12 +46,16 @@ class RaceMapFragment : BaseFragment<RaceMapViewModel, CourseEntry>(
         super.onViewCreated(view, savedInstanceState)
 
         initUi()
-
+        initListener()
 
     }
 
+    private fun initListener() {
+        binding.raceMapTraceBtn.setOnClickListener { startTracking() }
+    }
+
     private fun initUi(){
-        val mapView = MapView(requireActivity())
+        mapView = MapView(requireActivity())
         binding.raceMapView.addView(mapView)
 
     }
@@ -49,26 +64,17 @@ class RaceMapFragment : BaseFragment<RaceMapViewModel, CourseEntry>(
 
     }
 
-//    @SuppressLint("MissingPermission", "ServiceCast")
-//    private fun startTracking() {
-//        binding.raceMapView.currentLocationTrackingMode =
-//            MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading  //이 부분
-//
-//        val lm: LocationManager = getSystemService(requireContext().LOCATION_SERVICE) as LocationManager
-//        val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-//        //위도 , 경도
-//        val uLatitude = userNowLocation?.latitude
-//        val uLongitude = userNowLocation?.longitude
-//        val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
-//
-//        // 현 위치에 마커 찍기
-//        val marker = MapPOIItem()
-//        marker.itemName = "현 위치"
-//        marker.mapPoint =uNowPosition
-//        marker.markerType = MapPOIItem.MarkerType.BluePin
-//        marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
-//        binding.raceMapView.addPOIItem(marker)
-//    }
+    private fun startTracking() {
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+    }
+
+    // 위치추적 중지
+    private fun stopTracking() {
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+    }
+
+
+
 
 
 }
